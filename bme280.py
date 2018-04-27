bbfbf060e0650c638545d4f18cd3718513d6772a
Roland Smith <rsmith@xs4all.nl>
2018-04-28 01:05:26 +0200

    Add humidity measuremement.

--- bme280.py
+++ bme280.py
@@ -1,12 +1,12 @@
-# file: BMP280.py
+# file: bme280.py
 # vim:fileencoding=utf-8:fdm=marker:ft=python
 #
 # Copyright © 2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2018-04-08T22:38:40+0200
-# Last modified: 2018-04-27T13:42:25+0200
+# Last modified: 2018-04-28T01:04:42+0200
 """
-Code to use a BMP280 with FT232H using SPI.
+Code to use a BME280 with FT232H using SPI.
 The SPI interface provided by pyftdi is used.
 """
 
@@ -16,12 +16,14 @@ from time import sleep
 
 class Reg(IntEnum):
     ID = 0xD0
-    ID_VAL = 0x58  # Contents of the ID register for a BMP280.
+    ID_VAL = 0x60  # Contents of the ID register for a BME280.
+    CTRLHUM = 0xF2
     STATUS = 0xF3
     CONTROL = 0xF4
     CONFIG = 0xF5
     TEMP_MSB = 0xFA
     PRESS_MSB = 0xF7
+    HUMID_MSB = 0xFD
     # Compensation coefficient registers.
     T1 = 0x88
     T2 = 0x8A
@@ -35,33 +37,38 @@ class Reg(IntEnum):
     P7 = 0x9A
     P8 = 0x9C
     P9 = 0x9E
+    H1 = 0xA1
+    H2 = 0xE1
+    H3 = 0xE3
+    H45 = 0xE4
+    H6 = 0xE7
 
 
-class Bmp280spi:
+class Bme280spi:
     def __init__(self, spi):
-        """Create a Bmp280spi instance.
+        """Create a Bme280spi instance.
 
         Arguments:
             spi: SpiPort.
 
         >> from pyftdi.spi import SpiController
-        >> from BMP280 import Bmp280spi
+        >> from BME280 import Bme280spi
         >> ctrl = SpiController()
         >> ctrl.configure('ftdi://ftdi:232h/1')
         >> spi = ctrl.get_port(0)
         >> spi.set_frequency(1000000)
-        >> bmp280 = Bmp280spi(spi)
+        >> bmp280 = Bme280spi(spi)
 
         N.B: port 0 is D3 on the Adafruit FT232H!
         """
         self._spi = spi
         self._temp = None
         self._press = None
-        # Check if BMP280
+        self._humid = None
+        # Check if BME280
         if self._readU8(Reg.ID) != Reg.ID_VAL:
-            raise RuntimeError('Not a BMP280')
-        # Read the compensation coefficients. I'm reading the coefficients
-        # separately because reading them in one go failed.
+            raise RuntimeError('Not a BME280')
+        # Read the compensation coefficients.
         self._dig_T1 = float(self._readU16(Reg.T1))
         self._dig_T2 = float(self._readS16(Reg.T2))
         self._dig_T3 = float(self._readS16(Reg.T3))
@@ -74,18 +81,32 @@ class Bmp280spi:
         self._dig_P7 = float(self._readS16(Reg.P7))
         self._dig_P8 = float(self._readS16(Reg.P8))
         self._dig_P9 = float(self._readS16(Reg.P9))
+        self._dig_H1 = float(self._readU8(Reg.H1))
+        self._dig_H2 = float(self._readS16(Reg.H1))
+        self._dig_H3 = float(self._readU8(Reg.H3))
+        E4, E5, E6 = self._spi.exchange([Reg.H4 | 0x80], 3)
+        self._dig_H4 = float((E4 << 4) | (E5 & 0x0F))
+        self._deg_H5 = float((E6 << 4) | (E5 >> 4))
+        self._dig_H6 = float(self._readS8(Reg.H6))
 
     def _readU8(self, register):
         """Read an unsigned byte from the specified register"""
         return self._spi.exchange([register | 0x80], 1)[0]
 
+    def _readS8(self, register):
+        """Read a signed byte from the specified register"""
+        result = self._readU8(register)
+        if result > 128:
+            result -= 256
+        return result
+
     def _readU16(self, register):
-        """Read an unsigned short from the specified register"""
+        """Read an unsigned short starting at specified register"""
         data = self._spi.exchange([register | 0x80], 2)
         return data[1] << 8 | data[0]
 
     def _readS16(self, register):
-        """Read an unsigned short from the specified register"""
+        """Read an unsigned short starting at specified register"""
         result = self._readU16(register)
         if result > 32767:
             result -= 65536
@@ -112,7 +133,13 @@ class Bmp280spi:
             'dig_P6': self._dig_P6,
             'dig_P7': self._dig_P7,
             'dig_P8': self._dig_P8,
-            'dig_P9': self._dig_P9
+            'dig_P9': self._dig_P9,
+            'dig_H1': self._dig_H1,
+            'dig_H2': self._dig_H2,
+            'dig_H3': self._dig_H3,
+            'dig_H4': self._dig_H4,
+            'dig_H5': self._dig_H5,
+            'dig_H6': self._dig_H5
         }
 
     @property
@@ -130,14 +157,20 @@ class Bmp280spi:
         """The last measured pressure in mbar"""
         return 1000 * (self._press / 1.013e2)
 
+    @property
+    def humidity(self):
+        """The last measured relative humidity in %."""
+        return self._humid
+
     def read(self):
         """Read the sensor data from the chip and return (temperature, pressure)."""
         # Do one measurement in high resolution, forced mode.
+        self._spi.exchange([Reg.CTRLHUM & ~0x80, 0x87])
         self._spi.exchange([Reg.CONTROL & ~0x80, 0xFE])
         # Wait while maesurement is running
-        while self._readU8(Reg.STATUS) & 0x08:
+        while self._readU8(Reg.STATUS) != 0:
             sleep(0.01)
-        # Now read and process the data.
+        # Now read and process the temperature.
         UT = self._readU24(Reg.TEMP_MSB)
         # print("DEBUG: UT = ", UT)
         var1 = (UT / 16384.0 - self._dig_T1 / 1024.0) * self._dig_T2
@@ -149,7 +182,7 @@ class Bmp280spi:
         # print("DEBUG: t_fine = ", t_fine)
         self._temp = t_fine / 5120.0
         # print("DEBUG: self._temp = ", self._temp)
-        # Read pressure.
+        # Read and process pressure.
         UP = self._readU24(Reg.PRESS_MSB)
         # print("DEBUG: UP = ", UP)
         var1 = t_fine / 2.0 - 64000.0
@@ -178,4 +211,16 @@ class Bmp280spi:
         p = p + (var1 + var2 + self._dig_P7) / 16.0
         self._press = p
         # print("DEBUG: self._press = ", self._press)
-        return (self._temp, self._press)
+        # Read and process the relative humidity
+        adc_H = self._readU16(Reg.HUMID_MSB)
+        var_H = t_fine - 76800.0
+        var_H = ((adc_H - (self._dig_H4 * 64.0 + self._dig_H5 / 16384.0 * var_H)) *
+                 (self._dig_H2 / 65536.0 * (1.0 + self._dig_H6 / 67108864 * var_H *
+                                            (1.0 + self._dig_H3 / 67108864.0 * var_H))))
+        var_H = var_H * (1.0 - self._dig_H1 * var_H / 524288.0)
+        if var_H > 100.0:
+            var_H = 100.0
+        elif var_H < 0.0:
+            var_H = 0.0
+        self._humid = var_H
+        return (self._temp, self._press, self._humid)
